636b30a486b45bb609103730e6518c9f5de4045a
Andrei Marhatau <margatov.andrey@gmail.com>
2020-08-01 22:35:49 +0300

    Fix minor comments from expert for webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,13 +2,6 @@ const { merge } = require('webpack-merge');
 const path = require("path");
 const common = require("./webpack.config");
 
-const PATH = {
-    SRC: path.join(__dirname, 'src'),
-    PUBLIC: path.join(__dirname, 'src', 'public'),
-    DIST: path.join(__dirname, 'dist'),
-  };
-
 module.exports = merge(common, {
-  mode: 'development',
   devtool: 'source-map',
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,14 +5,7 @@ const common = require("./webpack.config");
 //Plugin to minimize js code
 const TerserJSPlugin = require('terser-webpack-plugin');
 
-const PATH = {
-    SRC: path.join(__dirname, 'src'),
-    PUBLIC: path.join(__dirname, 'src', 'public'),
-    DIST: path.join(__dirname, 'dist'),
-  };
-
 module.exports = merge(common, {
-    mode: 'production',
     optimization: {
         minimize: true,
         minimizer: [new TerserJSPlugin()]
