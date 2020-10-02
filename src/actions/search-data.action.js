c2414333f1cb361941a880b2b22916fb0e70cda5
Andrei Marhatau <margatov.andrey@gmail.com>
2020-10-02 17:20:10 +0300

    fix some code style issues

--- src/actions/search-data.action.js
+++ src/actions/search-data.action.js
@@ -2,18 +2,13 @@ const { createAction } = require("redux-actions");
 
 export const setSearchDataAction = createAction('SET_SEARCH_DATA');
 
-export const setSort = (sortBy, sortOrder) => {
-  return setSearchDataAction({sortBy: sortBy, sortOrder: sortOrder});
-}
-export const setSearch = (search, searchBy) => {
-  return setSearchDataAction({search: search, searchBy: searchBy});
-}
-export const setFilter = (data) => {
-  return setSearchDataAction({filter: data});
-}
-export const setOffset = (data) => {
-  return setSearchDataAction({offset: data});
-}
-export const setLimit = (data) => {
-  return setSearchDataAction({limit: data});
-}
+export const setSort = (sortBy, sortOrder) =>
+  setSearchDataAction({sortBy, sortOrder});
+export const setSearch = (search, searchBy) =>
+  setSearchDataAction({search, searchBy});
+export const setFilter = (filter) =>
+  setSearchDataAction({filter});
+export const setOffset = (offset) =>
+  setSearchDataAction({offset});
+export const setLimit = (limit) =>
+  setSearchDataAction({limit});
